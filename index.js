f089e86790a2077c2deb85fb6d80e82cba1ab070
Abhinav <ajagekarajagekar@gmail.com>
2023-10-15 04:05:50 +0530

    setting up ejs as view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+// view engine set up
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in starting Server: ${err}`);
