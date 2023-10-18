52e0c662a819d3039554995227c89786a68be8e9
Abhinav <ajagekarajagekar@gmail.com>
2023-10-18 17:44:07 +0530

    created sign-up using manual Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user')
+
 // render signUp page
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up', {
@@ -10,4 +12,39 @@ module.exports.signIn = function(req, res){
     return res.render('user_sign_in',{
         title: "Placement Cell | Sing IN"
     })
+}
+
+// get the sing up data
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email})
+    .then( user=>{
+        if(!user){
+            User.create(req.body)
+            .then((user)=>{
+                return res.redirect('/users/sign-in');
+            })
+            .catch(err =>{
+                if(err){console.log('error in creating user while singning up'); return}
+                return res.redirect('back');
+
+            })
+        }else{
+            console.log('User is alredy present please sing IN');
+            return res.redirect('back');
+        }  
+    })
+    .catch(err =>{
+        console.log('error in finding user in singing up', err);
+        return res.redirect('back');
+    })
+}
+
+
+// sing in and create session for the user
+module.exports.createSession = function(req, res){
+    // todo later
 }
